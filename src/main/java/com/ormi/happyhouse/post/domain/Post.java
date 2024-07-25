2ab281d0af0033a4cad97edaa6a97cb13e4d7949
nigoleto <nigoleto@gmail.com>
2024-07-25 09:38:03 +0900

    Feat: 게시글(Post) 엔티티 클래스 생성

--- src/main/java/com/ormi/happyhouse/post/domain/Post.java
+++ src/main/java/com/ormi/happyhouse/post/domain/Post.java
@@ -1,4 +1,38 @@
 package com.ormi.happyhouse.post.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Long viewCount;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    private boolean deleteYN;
+
+    private boolean noticeYN;
 }
