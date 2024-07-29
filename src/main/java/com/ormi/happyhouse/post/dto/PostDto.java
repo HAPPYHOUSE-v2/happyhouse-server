bb7bc5800acc6d647c41b431af1c3728ecfbbe7c
nigoleto <nigoleto@gmail.com>
2024-07-29 14:34:18 +0900

    chore: PostDto 필드 생성

--- src/main/java/com/ormi/happyhouse/post/dto/PostDto.java
+++ src/main/java/com/ormi/happyhouse/post/dto/PostDto.java
@@ -1,4 +1,25 @@
 package com.ormi.happyhouse.post.dto;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class PostDto {
+
+    private Long id;
+
+    private Long userId;
+
+    private String title;
+
+    private String content;
+
+    private Long viewCount;
+
+    private LocalDate createdAt;
+
+    private Date updatedAt;
+
+    private boolean deleteYN;
+
+    private boolean noticeYN;
 }
