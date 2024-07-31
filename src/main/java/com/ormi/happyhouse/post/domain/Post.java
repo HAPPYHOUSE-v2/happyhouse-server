bff46e9b5821dd7b098a6939958a4bbb35ccd1a4
nigoleto <nigoleto@gmail.com>
2024-07-31 10:56:42 +0900

    feat: Post 엔티티에 Column 애노테이션 추가

--- src/main/java/com/ormi/happyhouse/post/domain/Post.java
+++ src/main/java/com/ormi/happyhouse/post/domain/Post.java
@@ -15,21 +15,30 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "post_id")
+    private Long postId;
 
+    @Column(name = "user_id", nullable = false)
     private Long userId;
 
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private String content;
 
+    @Column(name = "view_count", nullable = false)
     private Long viewCount;
 
+    @Column(name = "created_at", nullable = false)
     private Date createdAt;
 
+    @Column(name = "updated_at")
     private Date updatedAt;
 
+    @Column(name = "delete_yn", nullable = false)
     private boolean deleteYn;
 
+    @Column(name = "notice_yn", nullable = false)
     private boolean noticeYn;
 }
