10a11542f3b81a4a15f987017a9400d4e1114adb
nigoleto <nigoleto@gmail.com>
2024-07-31 15:17:25 +0900

    feat: 게시글 생성 & 조회 기능 구현

--- src/main/java/com/ormi/happyhouse/post/controller/PostController.java
+++ src/main/java/com/ormi/happyhouse/post/controller/PostController.java
@@ -1,7 +1,17 @@
 package com.ormi.happyhouse.post.controller;
 
+import com.ormi.happyhouse.post.dto.PostDto;
 import com.ormi.happyhouse.post.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,4 +24,36 @@ public class PostController {
         this.postService = postService;
     }
 
+    @PostMapping
+    public String savePost(@RequestBody PostDto postDto, Model model) {
+        postService.savePost(postDto);
+        return "redirect:/post";
+    }
+
+    @GetMapping
+    public String showAllPost(
+            Model model
+            ,
+            @PageableDefault
+            @SortDefault.SortDefaults({
+                    @SortDefault(sort = "noticeYn", direction = Sort.Direction.DESC),
+                    @SortDefault(sort = "createdAt", direction = Sort.Direction.DESC)
+            })
+            Pageable pageable
+    ) {
+        Page<PostDto> posts = postService.showAllPost(pageable);
+
+        int nowPage = posts.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 2, 1);
+        int endPage = Math.min(startPage + 4, posts.getTotalPages());
+
+        model.addAttribute("posts", posts.getContent());
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("totalPage", posts.getTotalPages());
+        model.addAttribute("isSearchPage", false);
+
+        return "post/list";
+    }
 }
--- src/main/java/com/ormi/happyhouse/post/service/PostService.java
+++ src/main/java/com/ormi/happyhouse/post/service/PostService.java
@@ -1,7 +1,14 @@
 package com.ormi.happyhouse.post.service;
 
+import com.ormi.happyhouse.post.domain.Post;
+import com.ormi.happyhouse.post.dto.PostDto;
 import com.ormi.happyhouse.post.repository.PostRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class PostService {
@@ -13,4 +20,14 @@ public class PostService {
     }
 
 
+    public void savePost(PostDto postDto) {
+        postDto.setCreatedAt(new Date());
+        postDto.setViewCount(0L);
+        postRepository.save(postDto.toEntity());
+    }
+
+    public Page<PostDto> showAllPost(Pageable pageable) {
+        Page<Post> allPostPage = postRepository.findByDeleteYn(false, pageable);
+        return allPostPage.map(PostDto::fromEntity);
+    }
 }
